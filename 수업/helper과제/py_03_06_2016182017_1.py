32505305cb1c013ee33ccfb4f2a964cd373b2c5e
honey1586 <58782737+honey1586@users.noreply.github.com>
2020-09-19 22:34:58 +0900

    helper과제

--- 수업/helper과제/py_03_06_2016182017_1.py
+++ 수업/helper과제/py_03_06_2016182017_1.py
@@ -1,3 +1,4 @@
+from helper import *
 from pico2d import *
 
 # Boy와 관련된 것들을 클래스에 넣어서 관리하면 유지보수가 쉬워진다.
@@ -6,6 +7,7 @@ class Boy:
         self.x , self.y = get_canvas_width() // 2,80
         self.image = load_image('../res/run_animation.png')
         self.dx = 0
+        self.dy = 0
         self.frame = 0
 
     def draw(self):
@@ -13,6 +15,7 @@ class Boy:
 
     def update(self):
         self.x += self.dx * 5
+        self.y += self.dy * 5
         self.frame = (self.frame + 1) % 8
 
 class Grass:
@@ -23,23 +26,14 @@ class Grass:
         self.image.draw(self.x,self.y)
 
 def handle_events():
-    global running , dx ,x,y  #전역으로 바꿔준다.
+    global running , dx,dy,x,y  #전역으로 바꿔준다.
     evts = get_events()
     for e in evts:
         if e.type == SDL_QUIT:
             running = False
-        elif e.type == SDL_KEYDOWN:
-            if e.key == SDLK_ESCAPE:
-                running = False
-            elif e.key == SDLK_LEFT:
-                boy.dx -= 1
-            elif e.key == SDLK_RIGHT:
-                boy.dx += 1
-        elif e.type == SDL_KEYUP:
-            if e.key == SDLK_LEFT:
-                boy.dx += 1
-            elif e.key == SDLK_RIGHT:
-                boy.dx -= 1
+        elif e.type == SDL_MOUSEBUTTONDOWN:
+            move_toward((x,y),delta((x,y),(e.x,e.y),1)
+
 
         #elif e.type == SDL_MOUSEMOTION:
         #    boy.x,boy.y = e.x,get_canvas_height() - e.y - 1
