28efa75d45d0c2b60b47a5682859cb5c8c36c486
BDY <honey1586@naver.com>
2020-11-24 15:43:22 +0900

    위로 총쏘기 구현

--- Term_Project/bullet.py
+++ Term_Project/bullet.py
@@ -4,27 +4,34 @@ import gfw
 class Bullet:
     bullets = []
     def __init__(self, x,y,dir):
-        self.base_bullet_image = gfw.load_image('res/base_bullet.png')
-        self.baseR_bullet_image = gfw.load_image('res/base_bulletR.png')
+        self.base_bullet_image = gfw.load_image('res/base_bulletRIGHT.png')
+        self.baseR_bullet_image = gfw.load_image('res/base_bulletLEFT.png')
+        self.baseUP_bullet_image = gfw.load_image('res/base_bulletUP.png')
         self.x,self.y = x,y
         self.dir = dir
 
         # print('Radius = %d' % self.radius)
     def draw(self):
-        if self.dir == 1 or self.dir == 3:
-            self.base_bullet_image.clip_draw(0, 0, 150, 150, self.x + 20, self.y + 8, 350, 350)
-        elif self.dir == 0 or self.dir == 2:
-            self.baseR_bullet_image.clip_draw(0, 0, 150, 150, self.x + 20, self.y + 8, 350, 350)
+        if self.dir == 2:
+            self.base_bullet_image.clip_draw(0, 0, 150, 150, self.x + 30, self.y + 8, 350, 350)
+        elif self.dir == 1:
+            self.baseR_bullet_image.clip_draw(0, 0, 150, 150, self.x - 30, self.y + 8, 350, 350)
+        elif self.dir == 3:
+            self.baseUP_bullet_image.clip_draw(0, 0, 150, 150, self.x , self.y + 30, 350, 350)
 
     def update(self):
-        if self.dir == 1 or self.dir == 3:
-            self.x += 10
-        elif self.dir == 0 or self.dir == 2:
-            self.x -= 10
+        x,y = self.x,self.y
+        if self.dir == 2:
+            x += 10
+        elif self.dir == 1:
+            x -= 10
+        elif self.dir == 3:
+            y += 10
 
-        x = self.x
+        self.y = y
+        self.x = x
 
-        if x < -20 or x > get_canvas_width() + 20:
+        if x < -10 or x > get_canvas_width() + 10 or y > get_canvas_height() +10:
             Bullet.bullets.remove(self)
             print('Ball count - %d' % len(Bullet.bullets))
 
--- Term_Project/player.py
+++ Term_Project/player.py
@@ -13,11 +13,13 @@ class Player:
     BODY_LEFT_IDLE_ACTION = 3
     BODY_RIGHT_SHOOT_ACTION = 2
     BODY_LEFT_SHOOT_ACTION = 1
+    BODY_UP_SHOOT_ACTION = 5
 
-    LEG_RIGHT_IDLE_ACTION = 3
-    LEG_LEFT_IDLE_ACTION = 2
-    LEG_RIGHT_WALK_ACTION =1
-    LEG_LEFT_WALK_ACTION = 0
+    LEG_RIGHT_IDLE_ACTION = 4
+    LEG_LEFT_IDLE_ACTION = 3
+    LEG_RIGHT_WALK_ACTION =2
+    LEG_LEFT_WALK_ACTION = 1
+    LEG_JUMP_ACTION = 0
 
     #constructor
     def __init__(self):
@@ -26,14 +28,20 @@ class Player:
 
         self.body_fidx = 0
         self.leg_fidx = 0
-        self.ocha = 0
+        self.lxocha = 0
+        self.lyocha = 0
+        self.bxocha= 0
+        self.byocha = 0
         self.framespeed = 0
         self.temp = 0
+        self.tmp =0
+        self.dir = 0
 
         self.fireaction = False
         self.isLeftWalking = False
         self.isRightWalking = False
         self.isJump = False
+        self.isUpLook = False
 
         self.body_action = Player.BODY_RIGHT_IDLE_ACTION
         self.leg_action = Player.LEG_RIGHT_IDLE_ACTION
@@ -47,11 +55,11 @@ class Player:
     def draw(self):
         leg_sx = self.leg_fidx * 150
         leg_sy = self.leg_action * 150
-        self.leg_image.clip_draw(leg_sx, leg_sy, 150, 150, self.x - 5 + self.ocha, self.y - 28, 350, 350)
+        self.leg_image.clip_draw(leg_sx, leg_sy, 150, 150, self.x - 5 + self.lxocha, self.y - 28, 350, 350)
 
         body_sx = self.body_fidx * 150
         body_sy = self.body_action * 150
-        self.body_image.clip_draw(body_sx,body_sy,150,150,self.x,self.y,350,350)
+        self.body_image.clip_draw(body_sx,body_sy,150,150,self.x+self.bxocha,self.y + self.byocha,350,350)
 
     def update(self):
         global px
@@ -63,34 +71,61 @@ class Player:
             if self.fireaction == True:
                 self.body_action = Player.BODY_RIGHT_SHOOT_ACTION
             self.leg_action = Player.LEG_RIGHT_WALK_ACTION
-            self.ocha = 0
+            self.lxocha = 0
 
         if self.dx < 0:
             self.body_action = Player.BODY_LEFT_IDLE_ACTION
             if self.fireaction == True:
                 self.body_action = Player.BODY_LEFT_SHOOT_ACTION
             self.leg_action = Player.LEG_LEFT_WALK_ACTION
-            self.ocha = 10
+            self.lxocha = 10
 
         if self.isJump == True:
             if self.y <= 213:
+                self.leg_action = Player.LEG_JUMP_ACTION
                 self.y += 4
             if self.y > 213:
                 self.isJump = False
+
         if self.isJump == False and self.y > 125:
             self.y -= 4
+            if self.dir == 2:
+                self.leg_action = Player.LEG_RIGHT_IDLE_ACTION
+            if self.dir == 1:
+                self.leg_action = Player.LEG_LEFT_IDLE_ACTION
 
         px += self.dx
 
     def calframe(self):
         self.time += gfw.delta_time
         frame = self.time * 10
-        self.leg_fidx = int(frame) % 10
+
         if self.fireaction == False:
+            self.byocha = 0
+            self.bxocha = 0
             self.body_fidx = int(frame) % 10
 
         if self.fireaction == True:
             self.body_fidx = 5
+            if self.dir == 3 and self.tmp == 2:
+                self.bxocha = -5
+                self.byocha = 25
+                self.body_fidx = 0
+            elif self.dir == 3 and self.tmp == 1:
+                self.bxocha = 5
+                self.byocha = 25
+                self.body_fidx = 1
+
+        if self.isJump == False:
+            self.leg_fidx = int(frame) % 10
+        if self.isJump == True:
+            if self.dir == 2:
+                self.leg_fidx =0
+            if self.dir == 1:
+                self.leg_fidx =1
+
+
+
 
     # 움직임
     def moving(self):
@@ -100,35 +135,45 @@ class Player:
         self.isJump = True
 
 
-    def fire(self):
-        bullet = Bullet(self.x,self.y,self.leg_action)
+    def fire(self , dir):
+        bullet = Bullet(self.x,self.y,dir)
         Bullet.bullets.append(bullet)
 
     def tryfire(self):
-        self.fire()
         self.fireaction = True
 
-        if self.leg_action == Player.LEG_LEFT_IDLE_ACTION:
+        if self.dir == 1:
+
             self.body_action = Player.BODY_LEFT_SHOOT_ACTION
 
-        elif self.isRightWalking == True or self.leg_action == Player.LEG_RIGHT_IDLE_ACTION:
+        elif self.dir == 2:
+
             self.body_action = Player.BODY_RIGHT_SHOOT_ACTION
 
+        elif self.dir == 3:
 
+            self.body_action = Player.BODY_UP_SHOOT_ACTION
+
+
+
+        self.fire(self.dir)
 
     def handle_event(self, e):
         if e.type == SDL_KEYDOWN:
             if e.key == SDLK_LEFT:
+                self.dir = 1  # 왼쪽
                 self.dx -= 1
 
             if e.key == SDLK_RIGHT:
+                self.dir = 2  # 오른쪽
                 self.dx += 1
+
             if e.key == SDLK_UP:
-                pass
+                self.tmp = self.dir
+                self.dir = 3
             if e.key == SDLK_a:
                 self.temp = self.body_action
                 self.tryfire()
-
             if e.key == SDLK_s:
                 self.jump()
 
@@ -140,7 +185,8 @@ class Player:
             if e.key == SDLK_RIGHT:
                 self.dx -= 1
                 self.leg_action = Player.LEG_RIGHT_IDLE_ACTION
-
+            if e.key == SDLK_UP:
+                self.dir = self.tmp
             if e.key == SDLK_a:
                 self.fireaction = False
                 self.body_action = self.temp
